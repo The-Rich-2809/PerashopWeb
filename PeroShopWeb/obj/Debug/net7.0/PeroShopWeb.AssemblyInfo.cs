199c9d2fc2f7c8e88ef79560fc6a2d731cb7ea06
AlexSAdeCV <alexacos@outlook.com>
2024-04-29 01:02:58 -0600

    Lemus en 4

--- PeroShopWeb/obj/Debug/net7.0/PeroShopWeb.AssemblyInfo.cs
+++ PeroShopWeb/obj/Debug/net7.0/PeroShopWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeroShopWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0544c9da19a608528daf639f4cb4aa57f40dfe93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+952f7e0654140264324358ed054115b5e1694f92")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeroShopWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeroShopWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
