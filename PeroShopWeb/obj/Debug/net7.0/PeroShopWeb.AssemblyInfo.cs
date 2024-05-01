2754026f1be285de36dd83894c7b70b770242ec4
AlexSAdeCV <alexacos@outlook.com>
2024-05-01 17:48:07 -0600

    Login
    Terminado login

--- PeroShopWeb/obj/Debug/net7.0/PeroShopWeb.AssemblyInfo.cs
+++ PeroShopWeb/obj/Debug/net7.0/PeroShopWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeroShopWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79964d156697e3764386a09a0a1c29d5c0020143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc8a6923cade4fb038b3cc273b97f6bac62ab89d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeroShopWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeroShopWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
