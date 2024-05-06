cb64b7fdd0f222c0142d8d1aee5ff4a33a21bab2
Ricardo Prado Remigio <97078181+The-Rich-2809@users.noreply.github.com>
2024-05-05 20:39:59 -0600

    ProductosAdmin
    Casi termino

--- PeroShopWeb/Controllers/AdminController.cs
+++ PeroShopWeb/Controllers/AdminController.cs
@@ -1,16 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PeroShopWeb.Models;
+using System.Linq;
+using System.Net.Sockets;
+using Microsoft.AspNetCore.Http;
+using PeroShopWeb.Providers;
+using PeroShopWeb.Helpers;
 
 namespace PeroShopWeb.Controllers
 {
     public class AdminController : Controller
     {
+        private HelperUploadFiles helperUpload;
         private readonly PerashopDB _contextDB;
 
-        public AdminController(PerashopDB perashopDB)
+        public AdminController(HelperUploadFiles helperUpload, PerashopDB perashopDB)
         {
             _contextDB = perashopDB;
+            this.helperUpload = helperUpload;
         }
+        public static int IdProducto { get; set; }
+        public static string CategoriaProducto { get; set; }
 
         public void Cookies()
         {
@@ -44,7 +53,6 @@ namespace PeroShopWeb.Controllers
             Cookies();
             return View(listausuarios);
         }
-
         [HttpGet]
         public IActionResult EditarUsuarios(string id)
         {
@@ -52,30 +60,100 @@ namespace PeroShopWeb.Controllers
             Cookies();
             return View(listausuarios);
         }
+        [HttpGet]
+        public IActionResult Productos()
+        {
+            List<Producto> listproductos = _contextDB.Producto.ToList();
+            Cookies();
+            return View(listproductos);
+        }
+        [HttpGet]
+        public IActionResult NuevoProducto()
+        {
+            List<Proveedores> proveedores = _contextDB.Proveedores.ToList();
+            return View(proveedores);
+        }
+        [HttpPost]
+        public IActionResult NuevoProducto(Producto producto)
+        {
+            Cookies();
+            ProductoModel productoModel = new ProductoModel(_contextDB);
+            if (productoModel.NuevoProducto(producto))
+                return RedirectToAction("Productos");
+            else
+                return View();
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> EditarUsuarios(IFormFile Imagen, Usuario usuario, string id, string Contrasena1, string Contrasena2)
-        //{
-        //    var usuarios = new UsuarioModel(_contextDB);
-        //    if (Imagen != null)
-        //    {
-        //        string nombreImagen = usuario.Correo + Imagen.FileName;
-        //        await this.helperUpload.UploadFilesAsync(Imagen, nombreImagen, Fold.Images);
-        //        usuario.DireccionImagen = "../Images/Usuarios/" + nombreImagen;
-        //    }
-        //    else
-        //    {
-        //        usuario.DireccionImagePerfil = FotoPerfil;
-        //    }
-        //    if (Contrasena1 != null)
-        //    {
-        //        usuario.Contrasena = Contrasena1;
-        //    }
+        }
+        [HttpGet]
+        public IActionResult EditarProducto(int Id, string Nombre, string Categoria )
+        {
+            Cookies();
+            ViewBag.Id = Id;
+            IdProducto = Id;
+            ViewBag.NombreProducto = Nombre;
+            CategoriaProducto = Categoria;
+            ViewBag.Categoria = Categoria;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AgregaCaracteristicas()
+        {
+            List<ProductoColorAlamacenamientoInter> listproductoInter = _contextDB.ProductoInter.ToList();
+            List<ProductoColor> listproductoColor = _contextDB.Colores.ToList();
+            List<ProductoAlmacenamiento> listproductoAlmacenamiento = _contextDB.Almacenamientos.ToList();
 
-        //    usuarios.EditarUsuario(usuario);
-        //    Cookies();
-        //    return RedirectToAction(nameof(Usuarios));
-        //}
-    }
+            var viewmodel = new ProductosViewModel
+            {
+                ProductosInter = listproductoInter,
+                ProductoColors = listproductoColor,
+                productoAlmacenamientos = listproductoAlmacenamiento
+            };
+            Cookies();
+            ViewBag.Categoria = CategoriaProducto;
+            return View(viewmodel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AgregaCaracteristicas(ProductoColorAlamacenamientoInter productointer, IFormFile[] Imagen)
+        {
+            ProductoModel productoModel = new ProductoModel(_contextDB);
+            productointer.idproducto = IdProducto;
+            if(CategoriaProducto != "Telefonos")
+            {
+                productointer.idalmacenamiento = 1;
+            }
+
+            string nombreImagen = + productointer.idproducto + "_" + productointer.idcolor + "_" + productointer.idalmacenamiento + "_" + Imagen[0].FileName;
+            await this.helperUpload.UploadFilesAsync(Imagen[0], nombreImagen, Folders.Productos);
+
+            productointer.RutaImagen = "../Images/Products/" + nombreImagen;
+            productoModel.NuevasCaracteristicas(productointer);
+            return RedirectToAction("Productos");
+        }
+
+
+            //[HttpPost]
+            //[ValidateAntiForgeryToken]
+            //public async Task<IActionResult> EditarUsuarios(IFormFile Imagen, Usuario usuario, string id, string Contrasena1, string Contrasena2)
+            //{
+            //    var usuarios = new UsuarioModel(_contextDB);
+            //    if (Imagen != null)
+            //    {
+            //        string nombreImagen = usuario.Correo + Imagen.FileName;
+            //        await this.helperUpload.UploadFilesAsync(Imagen, nombreImagen, Fold.Images);
+            //        usuario.DireccionImagen = "../Images/Usuarios/" + nombreImagen;
+            //    }
+            //    else
+            //    {
+            //        usuario.DireccionImagePerfil = FotoPerfil;
+            //    }
+            //    if (Contrasena1 != null)
+            //    {
+            //        usuario.Contrasena = Contrasena1;
+            //    }
+
+            //    usuarios.EditarUsuario(usuario);
+            //    Cookies();
+            //    return RedirectToAction(nameof(Usuarios));
+            //}
+        }
 }
--- PeroShopWeb/obj/Debug/net7.0/PeroShopWeb.AssemblyInfo.cs
+++ PeroShopWeb/obj/Debug/net7.0/PeroShopWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeroShopWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46edcd970605b77f0fe527ba3e17862fb3aeaae4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aba3f41fd3e5abe95c79763d3a76e32aa8a04c84")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeroShopWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeroShopWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
