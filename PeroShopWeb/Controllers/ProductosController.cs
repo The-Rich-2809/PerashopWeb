2fea0f36192d8d1378328f7abf7b072e6e362dbb
Ricardo Prado Remigio <97078181+The-Rich-2809@users.noreply.github.com>
2024-05-17 23:57:41 -0600

    Carrito ok

--- PeroShopWeb/Controllers/ProductosController.cs
+++ PeroShopWeb/Controllers/ProductosController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PeroShopWeb.Models;
 using System.Drawing;
+using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace PeroShopWeb.Controllers
 {
@@ -13,18 +15,72 @@ namespace PeroShopWeb.Controllers
             _ContextoDB = perashopDB;
         }
 
+        public int IdProducto { get; set; }
+        public int IdUser { get; set; }
+        public void Cookies()
+        {
+            var miCookie = HttpContext.Request.Cookies["MiCookie"];
+
+            if (miCookie != null)
+            {
+                List<Usuario> listaUsuarios = _ContextoDB.Usuario.ToList();
+                foreach (var user in listaUsuarios)
+                {
+                    if (miCookie == user.Correo)
+                    {
+                        IdUser = user.ID;
+                        ViewBag.Nombre = user.Nombre;
+                        ViewBag.Nivel = user.TipoUsuario;
+                        ViewBag.FotoPerfil = user.DireccionImagen;
+                    }
+                }
+            }
+        }
+
         [HttpGet]
-        public IActionResult DetallesProductos(int valor) 
+        public IActionResult DetallesProductos(int valor)
         {
             ViewBag.ID = valor;
+            IdProducto = valor;
             var producto = _ContextoDB.Producto.First(p => p.ID == valor);
-            return View(producto);
+            ViewBag.NombreProducto = producto.Nombre;
+            ViewBag.Categoria = producto.Categoria;
+
+            List<Producto> listaProductos = _ContextoDB.Producto.ToList();
+            List<ProductoColor> productoColors = _ContextoDB.Colores.ToList();
+            List<ProductoAlmacenamiento> productoAlmacenamientos = _ContextoDB.Almacenamientos.ToList();
+            List<ProductoColorAlamacenamientoInter> listaproint = _ContextoDB.ProductoInter.ToList();
+            var viewmodel = new ProductosViewModel
+            {
+                Productos = listaProductos,
+                ProductosInter = listaproint,
+                ProductoColors = productoColors,
+                productoAlmacenamientos = productoAlmacenamientos
+            };
+
+            var json = JsonConvert.SerializeObject(viewmodel);
+
+            return View(viewmodel);
         }
 
         [HttpPost]
-        public IActionResult DetallesProductos(Producto producto)
+        public IActionResult DetallesProductos(CarritoVenta carritoVenta)
         {
-            return View();
+            Cookies();
+            carritoVenta.idusuario = IdUser;
+            carritoVenta.Cambio = 1;
+            carritoVenta.Envio = "";
+
+            var insertarventas = new CarritoVenta[]
+            {
+                carritoVenta,
+            };
+
+            foreach (var u in insertarventas)
+                _ContextoDB.CarritoVenta.Add(u);
+
+            _ContextoDB.SaveChanges();
+            return RedirectToAction("Carrito");
         }
         [HttpGet]
         public IActionResult ListaProductos(string valor)
@@ -39,7 +95,23 @@ namespace PeroShopWeb.Controllers
                 ProductosInter = listaproint
             };
 
-            return View(viewmodel); 
+            return View(viewmodel);
+        }
+        [HttpGet]
+        public IActionResult Carrito()
+        {
+            Cookies();
+            List<CarritoVenta> carrito = _ContextoDB.CarritoVenta.ToList();
+            List<ProductoColorAlamacenamientoInter> listaproint = _ContextoDB.ProductoInter.ToList();
+            ViewBag.IdUser = IdUser;
+
+            var viewmodel = new ProductosViewModel
+            {
+                CarritoVentas = carrito,
+                ProductosInter = listaproint
+            };
+
+            return View(viewmodel);
         }
     }
 }
